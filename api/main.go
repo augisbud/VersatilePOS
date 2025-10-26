aee08ba1b7ba63596f34fac8f613c75629dec05b
Augustas Budnikas <augustas@budnikas.dev>
2025-10-26 15:07:41 +0200

    Add Swagger UI

--- api/main.go
+++ api/main.go
@@ -1,31 +1,14 @@
 package main
 
 import (
-    "net/http"
-
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
-type album struct {
-    ID     string  `json:"id"`
-    Title  string  `json:"title"`
-    Artist string  `json:"artist"`
-    Price  float64 `json:"price"`
-}
-
-var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-func getAlbums(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, albums)
-}
-
 func main() {
-    router := gin.Default()
-    router.GET("/albums", getAlbums)
+	router := gin.Default()
 
-    router.Run("localhost:8080")
+	registerHandlers(router)
+	registerSwagger(router)
+
+	router.Run("localhost:8080")
 }

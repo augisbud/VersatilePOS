433c8b121457b7b2cb9f921a2d652f5eb4cc2901
Adomas <52313943+Kvizas@users.noreply.github.com>
2025-11-19 18:19:55 +0200

    Add dotenv (#12)
    * Add dotenv
    
    * add example
    
    * db creds

--- api/database/main.go
+++ api/database/main.go
@@ -2,7 +2,9 @@ package database
 
 import (
 	"VersatilePOS/database/entities"
+	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -13,8 +15,39 @@ var DB *gorm.DB
 func Connect() {
 	var err error
 
-	// TODO: Use environment variables or a config file for these settings in production
-	DB, err = gorm.Open(postgres.Open("host=localhost user=postgres password=SuperSecretPassword dbname=VersatilePOS port=5432 sslmode=disable"), &gorm.Config{})
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+
+	user := os.Getenv("DB_USER")
+	if user == "" {
+		user = "postgres"
+	}
+
+	password := os.Getenv("DB_PASSWORD")
+	if password == "" {
+		password = "SuperSecretPassword"
+	}
+
+	dbname := os.Getenv("DB_NAME")
+	if dbname == "" {
+		dbname = "VersatilePOS"
+	}
+
+	port := os.Getenv("DB_PORT")
+	if port == "" {
+		port = "5432"
+	}
+
+	sslmode := os.Getenv("DB_SSLMODE")
+	if sslmode == "" {
+		sslmode = "disable"
+	}
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", host, user, password, dbname, port, sslmode)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database: ", err)
 	}
